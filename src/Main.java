9dcef77be412d4e1caed6065dbd13e33cc42bd9a
Xuân Đại <daik5xuanvinh@gmail.com>
2026-05-12 16:31:54 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     private String author;
     private double price;
 
-    // Method
     public void printInfo() {
         System.out.println("Title: " + title);
         System.out.println("Author: " + author);
